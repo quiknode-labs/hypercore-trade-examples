7ff7b17dd2d23469719a27b5330f02a651a82aa4
yaanakbr <sakbar@quiknode.io>
2026-02-26 16:21:07 -0500

    add hip3 missing

---
+++ rust/examples/hip3_order.rs
@@ -0,0 +1,46 @@
+use hyperliquid_api_examples::Client;
+use serde_json::json;
+
+const COIN: &str = "xyz:SILVER";
+
+#[tokio::main]
+async fn main() {
+    let client = Client::from_env();
+
+    let mut mid = client.get_hip3_mid(COIN).await;
+    if mid == 0.0 {
+        println!("Could not fetch {COIN} mid price, using fallback");
+        mid = 78.0;
+    }
+
+    let sz = format!("{:.2}", 10.0 / mid);
+    let buy_px = format!("{:.2}", mid * 1.03);
+
+    println!("{COIN} mid: ${mid:.2}");
+    println!("BUY {sz} @ {buy_px} (IOC, ~${:.2} notional)\n", sz.parse::<f64>().unwrap() * mid);
+
+    let res = client
+        .exchange(&json!({
+            "action": {
+                "type": "order",
+                "orders": [{"asset": COIN, "side": "buy", "price": buy_px, "size": sz, "tif": "ioc"}],
+            },
+        }))
+        .await;
+
+    let hash = res["hash"].as_str().unwrap();
+    let sig = client.sign_hash(hash).await;
+
+    let result = client
+        .exchange(&json!({
+            "action": res["action"],
+            "nonce": res["nonce"],
+            "signature": sig,
+        }))
+        .await;
+
+    println!(
+        "{}",
+        serde_json::to_string_pretty(&result["exchangeResponse"]).unwrap()
+    );
+}
